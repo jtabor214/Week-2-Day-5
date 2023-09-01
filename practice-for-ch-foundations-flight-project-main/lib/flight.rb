304d91882da6c7988c8056af3354135cfa8522a2
Jacob Tabor <jtabor214@gmail.com>
2023-09-01 12:12:06 -0400

    defined class Flight and completed Flight#initialize

---
+++ practice-for-ch-foundations-flight-project-main/lib/flight.rb
@@ -0,0 +1,12 @@
+class Flight
+
+
+    def initialize(flight_number, capacity)
+        @flight_number = flight_number
+        @capacity = capacity
+        @passengers = []
+    end
+    
+
+
+end
